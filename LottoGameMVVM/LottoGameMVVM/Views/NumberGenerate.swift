35be18e8a5505767f99133760a3c77af31cee6cf
syyoo <yeol2s@naver.com>
2023-12-14 06:37:04 +0900

    refactor:LottoGame MVVM Update FIRST

---
+++ LottoGameMVVM/LottoGameMVVM/Views/NumberGenerate.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoGameMVVM
+//
+//  Created by 유성열 on 12/14/23.
+//
+
+import UIKit
+
+class NumberGenerateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
